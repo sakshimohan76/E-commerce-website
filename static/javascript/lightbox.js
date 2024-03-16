732f910c6a775c7b45d36aa3809d02be1f383af5
Kaustubh Vats <55024026+kaustubh-vats@users.noreply.github.com>
2024-03-16 19:50:04 +0530

    enhanced ui and fixed few bugs

--- static/javascript/lightbox.js
+++ static/javascript/lightbox.js
@@ -1,6 +1,7 @@
 
-var fullImgBox = document.getElementById("fullImgBox");
-var fullImg = document.getElementById("fullImg");
+const fullImgBox = document.getElementById("fullImgBox");
+const fullImg = document.getElementById("fullImg");
+const header = document.querySelector(".nav");
 
 function openFullImg(pic) {
     fullImgBox.style.display = "flex";
@@ -10,6 +11,16 @@ function openFullImg(pic) {
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
+
+window.addEventListener('scroll', () => {
+    const scrolledAmount = window.scrollY;
+    const headerClassList = header.classList;
+    if(scrolledAmount > 100 && !headerClassList.contains("scrolledNav")) {
+        header.classList.add("scrolledNav")
+    } else if(scrolledAmount < 100 && headerClassList.contains("scrolledNav")) {
+        header.classList.remove("scrolledNav")
+    }
+})
 
 
 // Get references to the search input and search results container
@@ -25,20 +36,21 @@ const performSearch = () => {
     searchResults.innerHTML = ''; // Clear previous search results
 
     if (searchTerm === '') {
-        cards.forEach(card => {
-            card.style.display = 'block'; // Show all cards when search input is empty
-        });
+        searchResults.style.display = 'none';
         return;
+    } else {
+        searchResults.style.display = 'flex';
     }
 
     cards.forEach(card => {
         const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
         const cardDesc = card.querySelector('.card-text').textContent.toLowerCase();
         if (cardTitle.includes(searchTerm) || cardDesc.includes(searchTerm)) {
-            card.style.display = 'block'; // Show matching card
+            card.style.display = 'flex'; // Show matching card
             searchResults.appendChild(card.cloneNode(true)); // Append matching card to search results
         }
     });
+    lazyLoadImages(searchResults);
 
     if (searchResults.children.length === 0) {
         searchResults.innerHTML = '<p>No results found.</p>';
